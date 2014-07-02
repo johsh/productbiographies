d846ca06568f3d297bb88a410c25b23f83c3d0cf
http://www.martinvonlupin.de <mat@monkimac.fritz.box>
2014-07-02 23:11:48 +0200

    Distance function

--- prototyp/getTradeData.php
+++ prototyp/getTradeData.php
@@ -22,6 +22,7 @@ if (!$link = mysql_connect('localhost', 'root', 'root')) {
 	= $cattle_production = $chickens_production = $pigs_production
 	= 0;
 
+
 	/*
 	$showCattle = $_GET['showCattle'];
 	if($showCattle == true){
@@ -176,6 +177,25 @@ if (!$link = mysql_connect('localhost', 'root', 'root')) {
 		
 		return ($element_trade_mode == $requested_trade_mode);
 
+	}
+
+	function distance($latA, $lonA, $latB, $lonB)
+	{
+		//TAKEN FROM http://derickrethans.nl/spatial-indexes-calculating-distance.html
+        // convert from degrees to radians
+        $latA = deg2rad($latA); $lonA = deg2rad($lonA);
+        $latB = deg2rad($latB); $lonB = deg2rad($lonB);
+
+        // calculate absolute difference for latitude and longitude
+        $dLat = ($latA - $latB);
+        $dLon = ($lonA - $lonB);
+
+        // do trigonometry magic
+        $d =
+                sin($dLat/2) * sin($dLat/2) +
+                cos($latA) * cos($latB) * sin($dLon/2) *sin($dLon/2);
+        $d = 2 * asin(sqrt($d));
+        return $d * 6371;
 	}
 
 	$total_import = $cattle_import + $chickens_import + $pigs_import;
